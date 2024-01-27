29264bb8fb8d333265e131cd07687232651e11ca
Orifha1 <orifhatshikotshi1@gmail.com>
2024-01-27 22:22:43 +0200

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/extensions': 'off',
+    'no-console': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import { config } from 'dotenv';
 
-dotenv.config()
+config();
 
 const app: Express = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) =>{
-    res.send('Hello');
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello');
 });
 
-app.listen(port, () =>{
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
